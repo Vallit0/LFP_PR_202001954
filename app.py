8adb4ac7f9dc536e6302643b6c725ba0d6d8ca91
Vallit0 <79114580+Vallit0@users.noreply.github.com>
2022-02-09 22:33:41 -0600

    Analizador Lexico

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 
 from typing import List
 from afd import automata
-from analizador import analizador
+#from analizador import analizador
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
 from tkinter import filedialog as fd
 
-def menu():
+if __name__ == '__main__':
     while True:
 
         print("=============Menu =====================")
@@ -20,19 +20,29 @@ def menu():
         op = input('Ingresa una opción: ')
         if op == '1':
             print("===Ha escogido Cargar Data====")
+
             Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
             filename = fd.askopenfilename(title="Select file", filetypes=(("Data Files", "*.data"), ("All", "*.txt")))
             print(filename)
-            current_file = filereader.read(filename)
-            tokens, errs = automata(current_file)
+            file = open(filename, encoding="utf8")
+            content = file.read()
+            print(content)
+            inventario,products, errs = automata(content)
+            print(inventario.year), print(products[0].units)
+            file.close()
+
+
+
+            
             #process_file(tokens, errs)
         elif op == '2':
             print("===Cargar Instrucciones====")
             filename = askopenfilename()
+            filereader = ''
             filereader = open(filename, 'r+', encoding='utf-8')
             current_file = filereader.read()
             tokens, errs = automata(current_file)
-            images = analizador(tokens)
+            #images = analizador(tokens)
             #for img in images:
                 #lst_images.append(img)
 
@@ -61,5 +71,5 @@ def menu():
             print("==================================")
             break
 
-if __name__ == '__main__':
-    menu()
+
+
---
+++ models/inventario.py
@@ -0,0 +1,6 @@
+
+class Inventario:
+    def __init__(self, month: str, year: str, products: list) -> None:
+        self.month: str = month
+        self.year: str = year
+        self.products: list = products
