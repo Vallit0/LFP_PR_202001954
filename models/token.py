26f08ad44a677de1e3847070958a12c620e210ca
Vallit0 <79114580+Vallit0@users.noreply.github.com>
2022-02-09 22:42:55 -0600

    Delete token.py

--- models/token.py
+++
@@ -1,6 +0,0 @@
-class Token:
-    def __init__(self, token: str, lexema: str, fila: int, col: int) -> None:
-        self.token: str = token
-        self.lexema: str = lexema
-        self.fila: int = fila
-        self.col: int = col
