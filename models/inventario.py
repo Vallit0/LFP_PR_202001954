0207acfe8f459ab365bd8560733b4128c6825e52
Vallit0 <79114580+Vallit0@users.noreply.github.com>
2022-02-16 16:32:07 -0600

    lol

--- models/inventario.py
+++
@@ -1,6 +0,0 @@
-
-class Inventario:
-    def __init__(self, month: str, year: str, products: list) -> None:
-        self.month: str = month
-        self.year: str = year
-        self.products: list = products
--- models/product.py
+++
@@ -1,5 +0,0 @@
-class Product:
-    def __init__(self, name: str, price: float, units: int) -> None:
-        self.name:  str = name 
-        self.price: int = price
-        self.units: int = units
